d72abc130d28b22704755db510a971a61644b7a3
Nikolaos Kostopoulos <nikolaos.kostopoulos@guthand.com>
2020-12-07 23:05:44 +0100

    day 2

--- 2020/Day_02/code.php
+++ 2020/Day_02/code.php
@@ -7,12 +7,47 @@ $input = readInput(__DIR__);
 // Task code
 function part01(array $input)
 {
+    $valid =0;
+    foreach ($input as $passwordData) {
+        $pwParts = explode(' ', $passwordData);
+        $rule = explode( '-', $pwParts[0]);
+        $needle = $pwParts[1][0];
+        $password = $pwParts[2];
+        $count = substr_count($password, $needle);
 
+        if ($count >= $rule[0] && $count <= $rule[1]) {
+            $valid++;
+        }
+    }
+
+    return $valid;
 }
 
 function part02(array $input)
 {
+    $valid =0;
+    foreach ($input as $passwordData) {
+        $pwParts = explode(' ', $passwordData);
+        $rule = explode( '-', $pwParts[0]);
+        $needle = $pwParts[1][0];
+        $password = $pwParts[2];
+        $count = substr_count($password, $needle);
 
+        $pos1 = (int)$rule[0] -1;
+        $pos2 = (int)$rule[1] -1;
+        $value1 = $password[$pos1];
+        $value2 = $password[$pos2];
+
+        if (($count > 1) && ($value1 === $needle && $value2 === $needle)) {
+            continue;
+        }
+
+        if ($value1 === $needle || $value2 === $needle) {
+            $valid++;
+        }
+    }
+
+    return $valid;
 }
 
 // Execute
